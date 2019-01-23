d1654b1016807a95c0df7b6438ebcbd293262163
mrivandu <mrivandu@hotmail.com>
2019-01-23 19:49:12 +0800

    Update。

--- KubernetesInstall-03.sh
+++ KubernetesInstall-03.sh
@@ -2,13 +2,15 @@
 # Download relevant softwares. Please verify sha512 yourself.
 while true;
     do
-        curl -L -C - -O https://dl.k8s.io/v1.13.2/kubernetes-server-linux-amd64.tar.gz>&/dev/null && \
-        curl -L -C - -O https://github.com/etcd-io/etcd/releases/download/v3.2.26/etcd-v3.2.26-linux-amd64.tar.gz>&/dev/null && \
-        curl -L -C - -O https://pkg.cfssl.org/R1.2/cfssl_linux-amd64>&/dev/null && \
-        curl -L -C - -O https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64>&/dev/null && \
-        curl -L -C - -O https://pkg.cfssl.org/R1.2/cfssl-certinfo_linux-amd64>&/dev/null &&\
+        echo "Downloading, please wait a moment." && \
+        curl -L -C - -O https://dl.k8s.io/v1.13.2/kubernetes-server-linux-amd64.tar.gz && \
+        curl -L -C - -O https://github.com/etcd-io/etcd/releases/download/v3.2.26/etcd-v3.2.26-linux-amd64.tar.gz && \
+        curl -L -C - -O https://pkg.cfssl.org/R1.2/cfssl_linux-amd64 && \
+        curl -L -C - -O https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64 && \
+        curl -L -C - -O https://pkg.cfssl.org/R1.2/cfssl-certinfo_linux-amd64
         if [ $? -eq 0 ];
-            echo "All software packages has been download"
-            then break; 
-        fi;
+            then 
+                echo "Congratulations! All software packages have been downloaded."
+                break
+        fi
     done
