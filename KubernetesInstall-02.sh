c2a2cee2ed2246119ff93ed80ee3d5be41c3e2ce
mrivandu <mrivandu@hotmail.com>
2019-01-23 19:29:18 +0800

    Update an article.

--- KubernetesInstall-02.sh
+++ KubernetesInstall-02.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Install the Docker engine. This needs to be executed on every machine.
 curl http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo -o /etc/yum.repos.d/docker-ce.repo>&/dev/null
-fi [ $? -eq 0 ] ;
+if [ $? -eq 0 ] ;
     then 
         yum remove docker \
                       docker-client \
@@ -12,12 +12,13 @@ fi [ $? -eq 0 ] ;
                       docker-logrotate \
                       docker-selinux \
                       docker-engine-selinux \
-                      docker-engine>>&/dev/null
+                      docker-engine>&/dev/null
         yum list docker-ce --showduplicates|grep "^doc"|sort -r
         yum -y install docker-ce-18.06.0.ce-3.el7
-        systemctl enable docker && systemctl start docker;
+        systemctl enable docker && systemctl start docker
         rm -f /etc/yum.repos.d/docker-ce.repo
+        systemctl status docker
     else
-        echo "Install failed! Please try again! " ;
+        echo "Install failed! Please try again! ";
         exit 110
 fi
--- KubernetesInstall-03.sh
+++ KubernetesInstall-03.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 # Download relevant softwares. Please verify sha512 yourself.
-curl -L -C - -O https://dl.k8s.io/v1.13.2/kubernetes-server-linux-amd64.tar.gz>&/dev/null
-curl -L -C - -O https://github.com/etcd-io/etcd/releases/download/v3.2.26/etcd-v3.2.26-linux-amd64.tar.gz>&/dev/null
-curl -L -C - -O https://pkg.cfssl.org/R1.2/cfssl_linux-amd64>&/dev/null
-curl -L -C - -O https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64>&/dev/null
-curl -L -C - -O https://pkg.cfssl.org/R1.2/cfssl-certinfo_linux-amd64>&/dev/null
+while true;
+    do
+        curl -L -C - -O https://dl.k8s.io/v1.13.2/kubernetes-server-linux-amd64.tar.gz>&/dev/null && \
+        curl -L -C - -O https://github.com/etcd-io/etcd/releases/download/v3.2.26/etcd-v3.2.26-linux-amd64.tar.gz>&/dev/null && \
+        curl -L -C - -O https://pkg.cfssl.org/R1.2/cfssl_linux-amd64>&/dev/null && \
+        curl -L -C - -O https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64>&/dev/null && \
+        curl -L -C - -O https://pkg.cfssl.org/R1.2/cfssl-certinfo_linux-amd64>&/dev/null &&\
+        if [ $? -eq 0 ];
+            echo "All software packages has been download"
+            then break; 
+        fi;
+    done
