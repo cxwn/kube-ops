10ffba6442e605627f14944dde361fe5b0ba4b8b
Ruihong Du (Chinasoft International) <v-ruidu@microsoft.com>
2019-01-23 16:11:34 +0800

    Create KubernetesInstall-01.sh

---
+++ KubernetesInstall-01.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#Every machine executes.
+#Add host domain name.
+cat>>/etc/hosts<<EOF
+172.31.2.11 gysl-master
+172.31.2.12 gysl-node1
+172.31.2.13 gysl-node2
+EOF
+#Modify related kernel parameters.
+cat>/etc/sysctl.d/kubernetes.conf<<EOF
+net.ipv4.ip_forward = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl -p /etc/sysctl.d/kubernetes.conf>&/dev/null
+#Turn off and disable the firewalld.
+systemctl stop firewalld
+systemctl disable firewalld
+#Disable the SELinux.
+sed -i 's/=enforcing/=disabled/' /etc/selinux/config
+#Disable the swap .
+sed -i 's/^.*swap/#&/g' /etc/fstab
+#Reboot
+reboot
