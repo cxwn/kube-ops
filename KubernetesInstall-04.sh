696fab9e0afa06b1408e8f65c9059fe297814fb6
mrivandu <mrivandu@hotmail.com>
2019-01-24 20:53:58 +0800

    Update some content.

--- KubernetesInstall-04.sh
+++ KubernetesInstall-04.sh
@@ -6,3 +6,61 @@ ls -l /usr/local/bin/
 tar -xzf etcd-v3.3.11-linux-amd64.tar.gz
 cp -p etcd-v3.3.11-linux-amd64/etc* /usr/local/bin/
 ls -l /usr/local/bin/
+# Create some CA certificates for etcd cluster.
+cat<<EOF>ca-config.json
+{
+  "signing": {
+    "default": {
+      "expiry": "87600h"
+    },
+    "profiles": {
+      "www": {
+         "expiry": "87600h",
+         "usages": [
+            "signing",
+            "key encipherment",
+            "server auth",
+            "client auth"
+        ]
+      }
+    }
+  }
+}
+EOF
+cat<<EOF>ca-csr.json
+{
+    "CN": "etcd CA",
+    "key": {
+        "algo": "rsa",
+        "size": 2048
+    },
+    "names": [
+        {
+            "C": "CN",
+            "L": "Beijing",
+            "ST": "Beijing"
+        }
+    ]
+}
+EOF
+cat<<EOF>server-csr.json
+{
+    "CN": "etcd",
+    "hosts": [
+    "172.31.3.11",
+    "172.31.3.12",
+    "172.31.3.13"
+    ],
+    "key": {
+        "algo": "rsa",
+        "size": 2048
+    },
+    "names": [
+        {
+            "C": "CN",
+            "L": "BeiJing",
+            "ST": "BeiJing"
+        }
+    ]
+}
+EOF
