5e6321e97a53c1c31f72cdfe5fe5e7e9bda9cbe3
mrivandu <mrivandu@hotmail.com>
2019-01-28 16:27:01 +0800

    Update KubernetesInstall-01.sh

--- KubernetesInstall-01.sh
+++ KubernetesInstall-01.sh
@@ -6,6 +6,7 @@ cat>>/etc/hosts<<EOF
 172.31.2.12 gysl-node1
 172.31.2.13 gysl-node2
 EOF
+
 # Modify related kernel parameters.
 cat>/etc/sysctl.d/kubernetes.conf<<EOF
 net.ipv4.ip_forward = 1
@@ -13,12 +14,16 @@ net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 EOF
 sysctl -p /etc/sysctl.d/kubernetes.conf>&/dev/null
+
 # Turn off and disable the firewalld.
 systemctl stop firewalld
 systemctl disable firewalld
+
 # Disable the SELinux.
 sed -i 's/=enforcing/=disabled/' /etc/selinux/config
-# Disable the swap .
+
+# Disable the swap.
 sed -i 's/^.*swap/#&/g' /etc/fstab
+
 # Reboot the machine.
 reboot
