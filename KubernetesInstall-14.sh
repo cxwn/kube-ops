bd4c425cd55422aac73e7d4a6aa5f453d57ea933
mrivandu <mrivandu@hotmail.com>
2019-01-30 20:34:40 +0800

    Update.

--- KubernetesInstall-14.sh
+++ KubernetesInstall-14.sh
@@ -1,3 +1,48 @@
 #!/bin/bash
-# Deploy the node.
-scp gysl-master:kubernetes/server/bin/{kube-proxy,kubelet,kubectl} /usr/local/bin/
+BOOTSTRAP_TOKEN=$(awk -F "," '{print $1}' /etc/kubernetes/token.csv)
+KUBE_SSL=/etc/kubernetes/ssl/
+KUBE_APISERVER="https://172.31.2.11:6443"
+
+cd $KUBE_SSL
+# Set cluster parameters.
+kubectl config set-cluster kubernetes \
+  --certificate-authority=./ca.pem \
+  --embed-certs=true \
+  --server=${KUBE_APISERVER} \
+  --kubeconfig=bootstrap.kubeconfig
+
+# Set client parameters.
+kubectl config set-credentials kubelet-bootstrap \
+  --token=${BOOTSTRAP_TOKEN} \
+  --kubeconfig=bootstrap.kubeconfig
+
+# Set context parameters. 
+kubectl config set-context default \
+  --cluster=kubernetes \
+  --user=kubelet-bootstrap \
+  --kubeconfig=bootstrap.kubeconfig
+
+# Set context.
+kubectl config use-context default --kubeconfig=bootstrap.kubeconfig
+
+# Create kube-proxy kubeconfig file. 
+
+kubectl config set-cluster kubernetes \
+  --certificate-authority=./ca.pem \
+  --embed-certs=true \
+  --server=${KUBE_APISERVER} \
+  --kubeconfig=kube-proxy.kubeconfig
+
+kubectl config set-credentials kube-proxy \
+  --client-certificate=./kube-proxy.pem \
+  --client-key=./kube-proxy-key.pem \
+  --embed-certs=true \
+  --kubeconfig=kube-proxy.kubeconfig
+
+kubectl config set-context default \
+  --cluster=kubernetes \
+  --user=kube-proxy \
+  --kubeconfig=kube-proxy.kubeconfig
+
+kubectl config use-context default --kubeconfig=kube-proxy.kubeconfig
+cd ~
---
+++ KubernetesInstall-15.sh
@@ -0,0 +1,2 @@
+
+scp gysl-master:kubernetes/server/bin/{kube-proxy,kubelet,kubectl} /usr/local/bin/
