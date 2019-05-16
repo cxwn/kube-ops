8d78aea8388a76573f3edc80195254007096ff12
mrivandu <mrivandu@hotmail.com>
2019-05-16 16:29:18 +0800

    Update.

--- kubernetes-all-in-one/kube_config.sh
+++ kubernetes-all-in-one/kube_config.sh
@@ -19,12 +19,13 @@ kube_version='v1.14.1'
 etcd_version='v3.3.13'
 flannel_version='v0.11.0'
 coredns_version='v1.5.0'
+docker_version='18.09.5-3.el7'
 master=( [gysl-master]='172.31.2.10' )
 node=( [gysl-node1]='172.31.2.11' [gysl-node2]='172.31.2.12' )
 etcd=( [etcd-master]='172.31.2.10' [etcd-01]='172.31.2.11' [etcd-02]='172.31.2.12' )
-bin='/usr/local/bin'
 kube_conf='/etc/kubernetes/conf.d'
 kube_ca='/etc/kubernetes/ca.d'
 etcd='/etc/etcd/conf.d'
 etcd_ca='/etc/etcd/ca.d'
-flanneld_conf='/etc/flanneld'
+flanneld_conf='/etc/flanneld.d'
+bin='/usr/local/bin'
