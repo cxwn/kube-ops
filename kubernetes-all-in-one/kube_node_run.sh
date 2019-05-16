ac00642caca23ed10421a244d7532530a6dc8db8
mrivandu <mrivandu@hotmail.com>
2019-05-16 17:03:22 +0800

    Update.

--- kubernetes-all-in-one/kube_node_run.sh
+++ kubernetes-all-in-one/kube_node_run.sh
@@ -21,42 +21,56 @@ for hostname in ${!node[@]};
 ${node[${hostname}]} ${hostname}
 EOF
     done
-read -p "Do you need init your system and install docker-engine?(Y/n)" affirm 
-if [[ "${affirm}" == 'y' || "${affirm}" == 'Y' ]]; then
-    echo "The system is initializing and installing docker-engine. Please waite a moment."
-    curl http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo -o /etc/yum.repos.d/docker-ce.repo>&/dev/null
-if [ $? -eq 0 ] ;
-    then
-        yum remove docker \
-                      docker-client \
-                      docker-client-latest \
-                      docker-common \
-                      docker-latest \
-                      docker-latest-logrotate \
-                      docker-logrotate \
-                      docker-selinux \
-                      docker-engine-selinux \
-                      docker-engine>&/dev/null
-        yum list docker-ce --showduplicates|grep "^doc"|sort -r
-        yum -y install docker-ce-18.09.5-3.el7
-        rm -f /etc/yum.repos.d/docker-ce.repo
-        systemctl enable docker --now && systemctl status docker
-    else
-        echo "Install failed! Please try again! ";
-        exit 110
-fi
+read -p "Do you need init your system and install docker-engine?(Y/n)" affirm
+while true;
+do 
+    if [[ "${affirm}"=='y' || "${affirm}"=='Y' ]]; 
+       then
 # Modify related kernel parameters.
-cat>/etc/sysctl.d/docker.conf<<EOF
+       cat>/etc/sysctl.d/docker.conf<<EOF
 net.ipv4.ip_forward = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 EOF
-sysctl -p /etc/sysctl.d/docker.conf>&/dev/null
+        sysctl -p /etc/sysctl.d/docker.conf>&/dev/null
 # Turn off and disable the firewalld.  
-systemctl stop firewalld  
-systemctl disable firewalld  
+        systemctl stop firewalld  
+        systemctl disable firewalld  
 # Disable the SELinux.  
-sed -i.bak 's/=enforcing/=disabled/' /etc/selinux/config  
+        sed -i.bak 's/=enforcing/=disabled/' /etc/selinux/config  
 # Disable the swap.  
-sed -i.bak 's/^.*swap/#&/g' /etc/fstab
+        sed -i.bak 's/^.*swap/#&/g' /etc/fstab
+        echo "The system is initializing and installing docker-engine. Please waite a moment."
+        curl http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo -o /etc/yum.repos.d/docker-ce.repo>&/dev/null
+        while true;
+        do
+            yum remove docker \
+                docker-client \
+                docker-client-latest \
+                docker-common \
+                docker-latest \
+                docker-latest-logrotate \
+                docker-logrotate \
+                docker-selinux \
+                docker-engine-selinux \
+                docker-engine>&/dev/null
+#        yum list docker-ce --showduplicates|grep "^doc"|sort -r
+                yum -y install docker-ce-${docker_version}
+                [ $? -eq 0 ] && rm -f /etc/yum.repos.d/docker-ce.repo
+                systemctl enable docker --now && systemctl status docker
+                if [ $? -eq 0 ]; 
+                    then
+                    echo "Install successfully. " && break 2
+                else
+                    echo "Install failed! Try again! "
+                    continue
+        done
+    elif [[  "${affirm}"=='n' || "${affirm}"=='N' ]];
+    then
+        echo 'Your system and docker-engine will not be modify! '
+        break
+    else
+        echo 'Your input is wrong. Please check! '
+        continue
+    done
 reboot
