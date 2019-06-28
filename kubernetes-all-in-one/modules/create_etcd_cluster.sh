fb190b3eef6e8b29f1076ddc6da86729c1afd135
mrivandu <mrivandu@hotmail.com>
2019-06-28 15:04:27 +0800

    Add some files.

---
+++ kubernetes-all-in-one/modules/create_etcd_cluster.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#===============================================================================
+#          FILE: create_etcd_cluster.sh
+#         USAGE: . ${YOUR_PATH}/create_etcd_cluster.sh 
+#   DESCRIPTION: 
+#        AUTHOR: IVAN DU
+#        E-MAIL: mrivandu@hotmail.com
+#        WECHAT: ecsboy
+#      TECHBLOG: https://ivandu.blog.csdn.net
+#        GITHUB: https://github.com/mrivandu
+#       CREATED: 2019-06-28 15:03:41
+#       LICENSE: GNU General Public License.
+#     COPYRIGHT: © IVAN DU 2019
+#      REVISION: v1.0
+#===============================================================================
+
+
+
---
+++ kubernetes-all-in-one/modules/create_kube_ca.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#===============================================================================
+#          FILE: create_kube_ca.sh
+#         USAGE: . ${YOUR_PATH}/create_kube_ca.sh 
+#   DESCRIPTION: 
+#        AUTHOR: IVAN DU
+#        E-MAIL: mrivandu@hotmail.com
+#        WECHAT: ecsboy
+#      TECHBLOG: https://ivandu.blog.csdn.net
+#        GITHUB: https://github.com/mrivandu
+#       CREATED: 2019-06-28 15:03:12
+#       LICENSE: GNU General Public License.
+#     COPYRIGHT: © IVAN DU 2019
+#      REVISION: v1.0
+#===============================================================================
+
+
+
