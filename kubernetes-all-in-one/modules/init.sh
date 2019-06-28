1515452d690c51d82b9e49bab7fd8ab9c38be71c
mrivandu <mrivandu@hotmail.com>
2019-06-28 15:12:43 +0800

    Add some files.

---
+++ kubernetes-all-in-one/modules/init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#===============================================================================
+#          FILE: init.sh
+#         USAGE: . ${YOUR_PATH}/init.sh 
+#   DESCRIPTION: 
+#        AUTHOR: IVAN DU
+#        E-MAIL: mrivandu@hotmail.com
+#        WECHAT: ecsboy
+#      TECHBLOG: https://ivandu.blog.csdn.net
+#        GITHUB: https://github.com/mrivandu
+#       CREATED: 2019-06-28 15:12:18
+#       LICENSE: GNU General Public License.
+#     COPYRIGHT: © IVAN DU 2019
+#      REVISION: v1.0
+#===============================================================================
+
+
+
