c01d2304a73948d890a45fc8dfeedf653f1ce799
mrivandu <mrivandu@hotmail.com>
2019-06-28 17:05:16 +0800

    Update

---
+++ kubernetes-all-in-one/modules/unzip_pkgs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#===============================================================================
+#          FILE: unzip_pkgs.sh
+#         USAGE: . ${YOUR_PATH}/unzip_pkgs.sh 
+#   DESCRIPTION: 
+#        AUTHOR: IVAN DU
+#        E-MAIL: mrivandu@hotmail.com
+#        WECHAT: ecsboy
+#      TECHBLOG: https://ivandu.blog.csdn.net
+#        GITHUB: https://github.com/mrivandu
+#       CREATED: 2019-06-28 17:03:22
+#       LICENSE: GNU General Public License.
+#     COPYRIGHT: © IVAN DU 2019
+#      REVISION: v1.0
+#===============================================================================
+
+# Unzip packages. 
+[ -d temp ] && rm -rf temp && mkdir temp
+[ ! -d temp ] && mkdir temp
+tar -xvzf pkgs/*.gz -C temp/
+
