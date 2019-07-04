50c9d4a4fba7ea85fdd6f2aac76f63ccd33e5006
mrivandu <mrivandu@hotmail.com>
2019-07-04 19:27:44 +0800

    Update

--- kubernetes-all-in-one/modules/init.sh
+++ kubernetes-all-in-one/modules/init.sh
@@ -34,7 +34,11 @@ for hostname in ${!hosts[@]}
     cat>>/etc/hosts<<EOF
 ${hosts["${hostname}"]} ${hostname}
 EOF
-  if [ ${hosts['gysl-master']} != ${hosts["${hostname}"]} ];then
-    rm -rf ${flanneld_conf} && mkdir -p ${flanneld_conf}
-  fi
+  done
+
+for flanneld_node in ${hosts[@]}
+  do
+   if [ ${flanneld_node} != ${hosts['gysl-master']} ];then
+     rm -rf ${flanneld_conf} && mkdir -p ${flanneld_conf}
+   fi
   done
